c4564bfaee5de34ddb7da457211883d0065bf9b8
Marc Zych <marczych@gmail.com>
2016-12-10 22:06:29 -0800

    Vim: Add Gundo plugin

--- addPlugin.sh
+++ addPlugin.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if [ "$#" != "1" ]
-then
+set -euo pipefail
+
+if [ "$#" != "1" ]; then
    echo "Usage: $0 <git url>"
    exit
 fi
 
 echo "Adding submodule..."
 
-REPONAME=$(echo "$1" | sed 's/^.*\///' | sed 's/\..*$//')
+repo_name=$(echo "$1" | sed 's/^.*\///' | sed 's/\..*$//')
 
-echo $REPONAME
+echo $repo_name
 
-git submodule add "$1" vim/bundle/$REPONAME
+git submodule add "$1" vim/bundle/"$repo_name"
 
 echo "Submodule added!"
