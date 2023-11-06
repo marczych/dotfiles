753711b9a9538536c277eb0ebbda525acafed6ba
Marc Zych <marc.zych@zoox.com>
2023-11-05 22:38:25 -0800

    Vim: Enabled remaining vimrc settings

--- nvim.lua
+++ nvim.lua
@@ -151,29 +151,11 @@ require('lazy').setup({
   },
 }, {})
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
 -- Enable break indent
 vim.o.breakindent = true
 
 -- Save undo history
 vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
@@ -337,8 +319,6 @@ local on_attach = function(_, bufnr)
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -361,7 +341,6 @@ end
 -- document existing key chains
 require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
@@ -446,6 +425,82 @@ cmp.setup {
   },
 }
 
+-- d and u make the window vertically smaller and bigger, respectively.
+vim.keymap.set('n', '<leader>d', '1000<C-W>-')
+vim.keymap.set('n', '<leader>u', '20<C-W>+')
+
+-- Open the current buffer in a new tab.
+vim.keymap.set('n', '<leader>t', '<cmd>tabe %<CR>')
+
+-- Open the current directory/folder in the current pane.
+vim.keymap.set('n', '<leader>f', '<cmd>Explore<CR>')
+
+-- Easier to get out of insert mode with 'kj'.
+vim.keymap.set('i', 'kj', '<Esc>')
+
+-- Automatically insert the closing brace.
+vim.keymap.set('i', '{<CR>', '{<CR>}<Esc>O')
+
+-- Live dangerously (disable all back up files).
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+-- Ignore case in search patterns by default.
+vim.opt.ignorecase = true
+-- Searches are case-sensitive if caps used.
+vim.opt.smartcase = true
+
+-- Reopen all changed files with :EA.
+vim.api.nvim_create_user_command('EA', 'checktime', {})
+
+-- Show dollar sign at end of text to be changed.
+vim.opt.cpoptions:append('$')
+
+-- Powerline already displays the mode.
+vim.opt.showmode = false
+
+-- Enable sane indent settings. Note: These can get overridden by EditorConfig.
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- Display some whitespace characters.
+vim.opt.listchars = {
+  tab = '>-',
+  trail = '.',
+  extends = '>',
+  precedes= '<',
+}
+vim.opt.list = true
+
+-- Highlight the current line.
+vim.opt.cursorline = true
+
+-- Don't always redraw (in macros, etc. in particular).
+vim.opt.lazyredraw = true
+
+-- Don't automatically resize windows when splitting.
+vim.opt.equalalways = false
+
+-- Keep 5 lines of context at the top and bottom of the cursor.
+vim.opt.scrolloff = 5
+
+-- Don't allow buffers to be hidden without prompting to save them.
+vim.opt.hidden = false
+
+-- Display a line at 80 columns.
+vim.opt.colorcolumn = '80'
+
+-- Enable dictionary completion and spell mode for prose files.
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'gitcommit', 'text' },
+  callback = function() vim.opt.spell = true end
+})
+-- autocmd FileType markdown,gitcommit,text setlocal complete+=k spell
+
 --[[
 -- Legacy Vimrc:
 " Plugin config.
@@ -466,106 +521,4 @@ call plug#end()
 
 let NERDTreeShowHidden=1
 
-" d and u make the window vertically smaller and bigger, respectively.
-nmap <Leader>d 1000<C-W>-
-nmap <Leader>u 20<C-W>+
-" Toggle paste mode.
-nmap <Leader>p :set invpaste<CR>:set paste?<CR>
-" Open the current buffer in a new tab.
-nmap <Leader>t :tabe %<CR>
-" Start FZF with a Ctrl-F.
-nmap <silent> <C-f> :FZF<CR>
-" Open the current directory/folder in the current pane.
-nmap <Leader>f :e %%/<CR>
-
-" Easier to get out of insert mode with 'kj'.
-imap kj <Esc>
-" Automatically insert the closing brace.
-imap {<CR> {<CR>}<Esc>O
-
-" In command mode, make %% expand to the directory that the current file is
-" in.
-cabbr <expr> %% expand('%:p:h')
-
-" General Vim settings.
-set encoding=utf-8
-behave xterm
-
-" Live dangerously (disable all back up files).
-set nobackup
-set nowritebackup
-set noswapfile
-
-" Reopen all changed files with :EA.
-command! EA :checktime
-" Automatically load changed files.
-set autoread
-
-" Highlight all search matches.
-set hlsearch
-" Immediately highlight search matches.
-set incsearch
-" Ignore case in search patterns by default.
-set ignorecase
-" Searches are case-sensitive if caps used.
-set smartcase
-
-" Show dollar sign at end of text to be changed.
-set cpoptions+=$
-" Allow backspacing without limits (in particular past the beginning of insert).
-set backspace=2
-
-" Disable error bells.
-set noerrorbells
-
-" Always show the status line.
-set laststatus=2
-" Enable modeline.
-set modeline
-" Powerline already displays the mode.
-set noshowmode
-
-" Enable sane indent settings. Note: These can get overridden by EditorConfig.
-set autoindent
-set smartindent
-set expandtab
-set tabstop=4
-set shiftwidth=4
-set softtabstop=4
-
-" Display some whitespace characters.
-set listchars=tab:>-,trail:.,extends:>,precedes:<
-set list
-
-" Highlight the current line.
-set cursorline
-" Don't always redraw (in macros, etc. in particular).
-set lazyredraw
-" Don't automatically resize windows when splitting.
-set noequalalways
-" Display row, column and % of document.
-set ruler
-" Keep 5 lines of context at the top and bottom of the cursor.
-set scrolloff=5
-" Display matching parens, brackets, etc.
-set showmatch
-" Show a better menu for autocompleting commands.
-set wildmenu
-" Soft wrap lines.
-set wrap
-" Don't allow buffers to be hidden without prompting to save them.
-set nohidden
-
-" Display a line at 80 columns.
-if exists('+colorcolumn')
-   set colorcolumn=80
-   highlight ColorColumn ctermbg=lightgrey guibg=lightgrey
-endif
-
-" Basic auto completion settings.
-set complete=.,w,b,u,t
-set dictionary=/usr/share/dict/words
-
-" Enable dictionary completion and spell mode for prose files.
-autocmd FileType markdown,gitcommit,text setlocal complete+=k spell
 --]]
