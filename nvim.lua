0e1d4a04173425bf3f4f4025660cd8bf06ec664e
Marc Zych <marc.zych@zoox.com>
2023-11-05 18:00:41 -0800

    Vim: Tweak language servers

--- nvim.lua
+++ nvim.lua
@@ -397,21 +397,9 @@ require('which-key').register {
 require('mason').setup()
 require('mason-lspconfig').setup()
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  pyright = {},
+  html = {},
 
   lua_ls = {
     Lua = {
