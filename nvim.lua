a83ae680d46cc357f2a41286d0231f2e69eb80a0
Marc Zych <marc.zych@zoox.com>
2023-11-07 21:52:04 -0800

    Vim: Simplify LSP setup

--- nvim.lua
+++ nvim.lua
@@ -268,7 +268,7 @@ local on_attach = function(_, bufnr)
 
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 
-  -- Create a command `:Format` local to the LSP buffer
+  -- Create a command `:Format` local to the LSP buffer.
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
@@ -278,39 +278,19 @@ end
 -- before setting up the servers.
 require('mason').setup()
 require('mason-lspconfig').setup()
-
-local servers = {
-  pyright = {},
-  html = {},
-
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
-}
-
 require('neodev').setup()
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
+mason_lspconfig.setup { }
 mason_lspconfig.setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
       on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
     }
   end,
 }
